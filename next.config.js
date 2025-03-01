7d562238ac13a1995ab74c37c67f7d8dbafa221a
jake-harris-prooduct <jakeharrisproduct@gmail.com>
2025-03-01 16:58:34 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/leonards-living/' : '',
+  basePath: isProd ? '/leonards-living' : '',
 };
